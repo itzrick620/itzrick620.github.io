49bed22ee540900c53725bf64a0154155f83a6b9
Ricky Garretson <rickygeeiii@outlook.com>
2023-10-11 01:24:45 -0400

    added form and other changes

--- projects/part1/success_story/script.js
+++ projects/part1/success_story/script.js
@@ -2,6 +2,11 @@ const toggleNav = () => {
     document.querySelector("main-nav").classList.toggle("show");
 };
 
+const printStory = () => {
+
+};
+
 window.onload = () => {
     document.getElementById("nav-toggle").addEventListener("click", toggleNav);
+    document.getElementById("button-add").onclick = printStory;
 };
