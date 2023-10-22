ec201887270ae16affaef8ed904f6a4578bb9ea5
Ricky Garretson <rickygeeiii@outlook.com>
2023-10-22 19:18:39 -0400

    added part 3 folders and iframe

---
+++ projects/part3/get_involved/script.js
@@ -0,0 +1,54 @@
+const toggleNav = () => {
+    document.getElementById("main-nav-items").classList.toggle("hidden");
+}
+
+const toggleHome = () => {
+    document.getElementById("p1of5").classList.toggle("hidden");
+    document.getElementById("p2of5").classList.add("hidden");
+    document.getElementById("p3of5").classList.add("hidden");
+    document.getElementById("p4of5").classList.add("hidden");
+    document.getElementById("p5of5").classList.add("hidden");
+}
+
+const toggleAdopt = () => {
+    document.getElementById("p1of5").classList.add("hidden");
+    document.getElementById("p2of5").classList.toggle("hidden");
+    document.getElementById("p3of5").classList.add("hidden");
+    document.getElementById("p4of5").classList.add("hidden");
+    document.getElementById("p5of5").classList.add("hidden");
+}
+
+const toggleDonate = () => {
+    document.getElementById("p1of5").classList.add("hidden");
+    document.getElementById("p2of5").classList.add("hidden");
+    document.getElementById("p3of5").classList.toggle("hidden");
+    document.getElementById("p4of5").classList.add("hidden");
+    document.getElementById("p5of5").classList.add("hidden");
+}
+
+const toggleInvolved = () => {
+    document.getElementById("p1of5").classList.add("hidden");
+    document.getElementById("p2of5").classList.add("hidden");
+    document.getElementById("p3of5").classList.add("hidden");
+    document.getElementById("p4of5").classList.toggle("hidden");
+    document.getElementById("p5of5").classList.add("hidden");
+}
+
+const toggleSuccess = () => {
+    document.getElementById("p1of5").classList.add("hidden");
+    document.getElementById("p2of5").classList.add("hidden");
+    document.getElementById("p3of5").classList.add("hidden");
+    document.getElementById("p4of5").classList.add("hidden");
+    document.getElementById("p5of5").classList.toggle("hidden");
+}
+
+window.onload = () => {
+    document.getElementById("nav-toggle").onclick = toggleNav;
+    document.getElementById("p1of5").onclick = toggleHome;
+    document.getElementById("p2of5").onclick = toggleAdopt;
+    document.getElementById("p3of5").onclick = toggleDonate;
+    document.getElementById("p4of5").onclick = toggleInvolved;
+    document.getElementById("p4of5").onclick = toggleSuccess;
+
+
+}
