e3c6fca002a469e14b776de842168246215122c1
Ricky Garretson <rickygeeiii@outlook.com>
2023-10-29 23:32:17 -0400

    fixed maybe??

--- projects/part4/success_story/script.js
+++ projects/part4/success_story/script.js
@@ -44,43 +44,46 @@ const showDogs = async () => {
 
 
 const getDog = (dog) => {
+    // Create a new section element
     let section = document.createElement("section");
     section.classList.add("dog");
 
+    // Create an h1 element for the dog's name
     let h1 = document.createElement("h1");
     h1.innerText = dog.name;
     section.appendChild(h1);
 
-    let h3 = document.createElement("h3");
-    h3.innerText = dog.desc;
-    section.appendChild(h3);
-
+    // Create a div for the details
     let details = document.createElement("div");
     details.classList.add("dogs-details");
     section.appendChild(details);
 
+    // Create an unordered list for the dog's information
     let ul = document.createElement("ul");
     details.appendChild(ul);
 
-    // Update to directly access properties from the 'dog' object
+    // Create list items for owner and email
+    let liOwner = document.createElement("li");
+    liOwner.innerText = "Owner: " + dog.owner;
+    ul.appendChild(liOwner);
+
+    let liEmail = document.createElement("li");
+    liEmail.innerText = "Email: " + dog.emailOwner;
+    ul.appendChild(liEmail);
+
+    // Create a list item for the description
     let liDesc = document.createElement("li");
     liDesc.innerText = dog.desc;
     ul.appendChild(liDesc);
 
-    let liOwner = document.createElement("li");
-    liOwner.innerText = `Owner: ${dog.owner}`;
-    ul.appendChild(liOwner);
-
-    let liEmail = document.createElement("li");
-    liEmail.innerText = `Owner's Email: ${dog.emailOwner}`;
-    ul.appendChild(liEmail);
-
+    // Create an image element for the dog's picture
     let img = document.createElement("img");
     img.src = dog.img;
     section.appendChild(img);
 
     return section;
 };
+
 
 
 window.onload = () => {
